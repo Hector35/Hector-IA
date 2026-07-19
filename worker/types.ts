124a745c78ca5bb7be362bdc8a2b532e268bcd17
Hector35 <Hectorhdz035@gmail.com>
2026-07-19 19:11:36 +0800

    Connect technical jobs to GitHub runner

--- worker/types.ts
+++ worker/types.ts
@@ -1,2 +1,2 @@
-export type Bindings = { DB: D1Database; FILES: R2Bucket; OPENAI_API_KEY: string; OPENAI_MODEL: string; APP_NAME: string; ASSETS: Fetcher };
+export type Bindings = { DB: D1Database; FILES: R2Bucket; OPENAI_API_KEY: string; GITHUB_RUNNER_TOKEN?: string; OPENAI_MODEL: string; APP_NAME: string; ASSETS: Fetcher };
 export type Variables = { userId: string; userName: string };
