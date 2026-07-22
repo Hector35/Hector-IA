a7ddb8a1d4e7fc2a9a7e52b9b9f22d075e1a2121
Hector35 <67966310+Hector35@users.noreply.github.com>
2026-07-22 08:54:36 -0600

    chore: record approved visual redesign

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// Deliberate Hector ASI evolution redesign, guarded by the visual-change-approved PR label.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HectorASIEvolutionApp} from './HectorASIEvolutionApp';
