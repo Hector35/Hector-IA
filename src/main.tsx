49b29199d02026cdc80a8ef17a5b63ad5ac3c3cd
Hector35 <67966310+Hector35@users.noreply.github.com>
2026-07-22 06:45:35 -0600

    Cargar estilos Markdown de Hector ASI

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HectorASIApp} from './HectorASIApp';
 import './hector-asi.css';
+import './hector-asi-markdown.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><HectorASIApp/></React.StrictMode>
